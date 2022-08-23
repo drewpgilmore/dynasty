7a19d6203efab0b187b9b537b810f1b88e056e63
drewpgilmore <drewpgilmore@gmail.com>
2022-08-23 11:24:57 -0700

    added functionality for scores to render through app

--- app.py
+++ app.py
@@ -4,11 +4,21 @@
 from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+# flask app
 app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+# app routes
+@app.route("/")
+def index(): 
+    # render live scoreboard with team & total points
+    # from scores import getScores
+    # scores = getScores()
+    return render_template('index.html')
+
+@app.route("/chronicle")
+def chronicle(): 
+    # render web version of chronicle
+    return render_template("chronicle.html")
+
--- scores.py
+++ scores.py
@@ -6,12 +6,16 @@ import numpy as np
 import time
 
 from espn_api.football import League
-import config
+from config import league_id, espn_s2, swid
 
 ED = League(
     league_id = league_id,
     year = 2022,
-    espn_s2=espn_s2
+    espn_s2=espn_s2,
     swid=swid
-)
+
+def getScores():
+    scores = pd.DataFrame(index=)
+    scoreboard = ED.scoreboard(week=ED.current_week)
+
 
