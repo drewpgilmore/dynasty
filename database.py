f97309fea5887c897da0d30d5414994a1ce1f7eb
drewpgilmore <drewpgilmore@gmail.com>
2022-10-12 10:23:16 -0700

    added db models for sqlite implementation

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/dynasty.db')
+engine = create_engine('sqlite:////tmp/dynasty.db', echo=True)
 db_session = scoped_session(
     sessionmaker(
         autocommit=False,
