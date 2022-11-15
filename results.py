c79037e16e273a6501af09a424480c30b117968d
drewpgilmore <drewpgilmore@gmail.com>
2022-11-15 14:58:39 -0800

    pulling owner name instead of team name

--- results.py
+++ results.py
@@ -1,12 +1,12 @@
 #! /env/bin python3
 # results.py -- script for retrieving weekly scores
 
-from scores import getLeague, getScores
+from scores import Dynasty
 import pandas as pd
 
-league = getLeague(2022)
+league = Dynasty(year=2022)
 week = int(input('Week: '))
-data = getScores(league.year, week)
+data = league.weekScores(week=week)
 
 df = pd.DataFrame(
     data=data.values(),
