88d70026d59881e128ada69326872a289fd9e743
drewpgilmore <drewpgilmore@gmail.com>
2023-02-09 12:35:08 -0800

    test update.sh and revert css to normal

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 # Updates EC2 instance after app updates pushed
-ssh -i ~/aws/fantasy-ubuntu-rsa.pem ubuntu@encinitasdynasty.com
-cd dynasty
-git pull
-sudo systemctl stop fantasy.service # stops serving app -- url will return 502 bad gateway
-sudo systemctl daemon-reload
-sudo systemctl start fantasy.service
-sudo systemctl enable fantasy.service
-echo "App updated!"
+ssh -i ~/aws/fantasy-ubuntu-rsa.pem ubuntu@encinitasdynasty.com "cd dynasty; 
+git pull; 
+sudo systemctl stop fantasy.service;
+sudo systemctl daemon-reload;
+sudo systemctl start fantasy.service;
+sudo systemctl enable fantasy.service;
+echo 'App updated!'"
