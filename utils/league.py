da7c730599ccaecefb9e2dc019a2071cfb5e6d07
Drew Gilmore <drewpgilmore@gmail.com>
2023-05-05 15:46:50 -0700

    initial commit for utils branch

---
+++ utils/league.py
@@ -0,0 +1,8 @@
+# /usr/bin/python3 
+# league.py
+# define league class for app
+
+class League {
+    def __init__(self): 
+	    pass
+}
---
+++ utils/scoreboard.py
@@ -0,0 +1,8 @@
+# /usr/bin/python3 
+# scoreboard.py
+# define scoreboard class for app
+
+class Scoreboard {
+    def __init__(self): 
+	    pass
+}
