5f65c88407bd4c308c0796845fb5673fdee34239
Drew Gilmore <drewpgilmore@gmail.com>
2023-05-05 14:52:23 -0700

    remove update.sh from repo

--- update.sh
+++
@@ -1,8 +0,0 @@
-# Updates EC2 instance after app updates pushed
-ssh -i ~/aws/fantasy-ubuntu-rsa.pem ubuntu@dynastyscoreboard.com "cd dynasty; 
-git pull; 
-sudo systemctl stop fantasy.service;
-sudo systemctl daemon-reload;
-sudo systemctl start fantasy.service;
-sudo systemctl enable fantasy.service;
-echo 'App updated!'"
